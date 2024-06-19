6272937a0a6ba941ff22b977e2eefc0b914f4f99
terry-ian <39936938+terry-ian@users.noreply.github.com>
2024-06-19 14:49:07 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,40 +1,13 @@
+# api/index.py
 
-from flask import Flask, request, jsonify, render_template
-import google.generativeai as genai
-import os
+from flask import Flask
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
-
-# 設定你的 API 金鑰
-api_key = os.getenv("GOOGLE_API_KEY")
-if not api_key:
-    raise ValueError("GOOGLE_API_KEY environment variable not set")
-
-# 初始化 Gemini API 客戶端
-genai.configure(api_key="AIzaSyD8OG39WRzCydCU2l6qOmqLJkldMbFmI9o")
-
-model = genai.GenerativeModel(model_name="gemini-1.5-pro")
-chat = model.start_chat(history=[])
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return 'Hello, World!'
 
-@app.route('/ask', methods=['POST'])
-def ask():
-    data = request.get_json()
-    question = data.get('question')
-
-    # 使用 Google Gemini API 生成答案
-    answer = generate_answer(question)
-
-    return jsonify({'answer': answer})
-
-def generate_answer(question):
-    # 使用 Google Gemini API 來生成答案
-    response = chat.send_message(question)
-    #response = model.generate_content(question)
-    return response.text.replace('**','').replace('*','-')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return 'About'
